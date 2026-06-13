d694f0c9273556e53eabfee0ee458fff592f5986
Addy <pooraddyy@gmail.com>
2026-06-13 11:50:30 +0530

    v0.1.2: fix file+image upload (refs field), remove broken model IDs

---
+++ kimi_nocost/__init__.py
@@ -0,0 +1,6 @@
+from .client import KimiClient
+from .errors import KimiAPIError, KimiAuthError, KimiError, KimiSessionLimitError, KimiUploadError
+from .models import Models
+
+__all__ = ["KimiClient", "KimiError", "KimiAuthError", "KimiAPIError", "KimiUploadError", "KimiSessionLimitError", "Models"]
+__version__ = "0.1.2"
